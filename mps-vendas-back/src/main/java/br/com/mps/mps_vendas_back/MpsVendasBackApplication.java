4ed61a15943bff06af5ec5bc9a533881fb41f182
Franklin <Franklin3143>
2022-01-21 22:22:42 -0300

    criação do repositório mps-vendas-back

---
+++ mps-vendas-back/src/main/java/br/com/mps/mps_vendas_back/MpsVendasBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.mps.mps_vendas_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpsVendasBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpsVendasBackApplication.class, args);
+    }
+
+}
---
+++ mps-vendas-back/src/test/java/br/com/mps/mps_vendas_back/MpsVendasBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mps.mps_vendas_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpsVendasBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
