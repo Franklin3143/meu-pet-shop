5072113f4df3b3e3c514630a98db3ac3fe88c546
Franklin <Franklin3143>
2022-01-21 22:11:09 -0300

    criação do repositório cadastro back

---
+++ mps-cadastro-back/src/main/java/br/com/mps_cadatro_back/MpsCadatroBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.mps_cadatro_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpsCadatroBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpsCadatroBackApplication.class, args);
+    }
+
+}
---
+++ mps-cadastro-back/src/test/java/br/com/mps_cadatro_back/MpsCadatroBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mps_cadatro_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpsCadatroBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
