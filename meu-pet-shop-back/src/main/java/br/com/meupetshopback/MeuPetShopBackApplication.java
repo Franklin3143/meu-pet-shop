af8ab8fff41755c0dc54ce552aaf2a1776f34f4a
Franklin <Franklin3143>
2022-01-21 22:01:19 -0300

    Commit inicial backend

---
+++ meu-pet-shop-back/src/main/java/br/com/meupetshopback/MeuPetShopBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.meupetshopback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPetShopBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeuPetShopBackApplication.class, args);
+    }
+
+}
