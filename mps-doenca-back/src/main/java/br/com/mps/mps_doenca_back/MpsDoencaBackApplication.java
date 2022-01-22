becb7884f374cd0d932cd31f8f5bcd4b3f2e1b27
Franklin <Franklin3143>
2022-01-21 22:15:46 -0300

    criação de repositório mps-doenca-back

---
+++ mps-doenca-back/src/main/java/br/com/mps/mps_doenca_back/MpsDoencaBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.mps.mps_doenca_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpsDoencaBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpsDoencaBackApplication.class, args);
+    }
+
+}
